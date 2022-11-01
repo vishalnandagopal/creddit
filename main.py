d6bf855410aaed260115aaa3d61328852f92390b
Vishal N <61023404+vishalnandagopal@users.noreply.github.com>
2022-11-02 02:03:35 +0530

    removed default sub-reddit, set during testing

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import src.terminal as terminal
 def job(subreddit:str="soccer"):
     terminal.print_subreddit_posts(subreddit)
 
-#subreddit = input("Enter the subreddit you want to visit: r/")
-job(subreddit="soccer")
+subreddit = input("Enter the subreddit you want to visit: r/")
+job(subreddit)

bc7a5deae6e180a2a787ebee345e1275c01c4bc6
Vishal N <61023404+vishalnandagopal@users.noreply.github.com>
2022-11-11 19:22:12 +0530

    fix3

--- src/readenv.py
+++ src/readenv.py
@@ -5,13 +5,15 @@ class GlobalVariables:
     def __init__(self):
         if "reddit-terminal.env" in listdir():
             load_dotenv("reddit-terminal.env")
-        else:
-            load_dotenv()
-        if getenv("IGNORED_USERS"):
-            self.ignored_users = getenv("IGNORED_USERS").split(",")
+            if getenv("IGNORED_USERS"):
+                self.ignored_users = getenv("IGNORED_USERS").split(",")
+            else:
+                self.ignored_users = []
+            self.posts_to_print = int(getenv("POSTS_TO_PRINT"))
+            if not self.posts_to_print:
+                self.posts_to_print = 10
+            self.default_subreddit = getenv("DEFAULT_SUBREDDIT")
         else:
             self.ignored_users = []
-        self.posts_to_print = int(getenv("POSTS_TO_PRINT"))
-        if not self.posts_to_print:
             self.posts_to_print = 10
-        self.default_subreddit = getenv("DEFAULT_SUBREDDIT")
+            self.default_subreddit = "soccer"
